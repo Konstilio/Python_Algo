5e8110d319f916b1684af87a623aff95c60f2ade
Aleksander Konstantinov <OleksandrKonstantinov@aleksanders-mbp.livatek.com>
2016-02-12 09:47:08 +0200

    Path Sum

---
+++ Path_sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    
+    def RecursiveSum(self, node, currSum, TargetSum):
+        if node == None:
+            return False
+        
+        currSum += node.val
+        if node.left == None and node.right == None:
+            return currSum == TargetSum
+            
+        return (self.RecursiveSum(node.left, currSum, TargetSum) or self.RecursiveSum(node.right, currSum, TargetSum))
+            
+    
+    def hasPathSum(self, root, sum):
+        return self.RecursiveSum(root, 0, sum)
