f35c872750f6b8650247937b5fbd90123b839342
Aleksander Konstantinov <OleksandrKonstantinov@Aleksanders-MacBook-Pro.local>
2016-02-13 17:46:52 +0200

    Remove Duplicates from Sorted Array II

---
+++ RemoveDuplicatesFromSortedArrayII.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        l = len(nums)
+        if l <  2:
+            return l
+        
+        goodIndex = 0
+        dupNum = 1
+        for k in range(1, l):
+            curr = nums[k]
+            good = nums[goodIndex]
+            
+            if curr > good:
+                nums[k], nums[goodIndex + 1] = nums[goodIndex + 1], nums[k]
+                goodIndex += 1
+                dupNum = 1
+                continue
+            
+            elif curr == good and dupNum == 1:
+                nums[k], nums[goodIndex + 1] = nums[goodIndex + 1], nums[k]
+                goodIndex += 1
+                dupNum = 2
+                continue
+
+  
+        return goodIndex + 1
